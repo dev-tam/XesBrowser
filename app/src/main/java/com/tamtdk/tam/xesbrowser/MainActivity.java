ffb9ac78b7d780297c54fcc75928b17c25232614
TamjidMd <tamjid.mdh@gmail.com>
2017-12-03 22:12:05 +0600

    Testing the App phone.

--- app/src/main/java/com/tamtdk/tam/xesbrowser/MainActivity.java
+++ app/src/main/java/com/tamtdk/tam/xesbrowser/MainActivity.java
@@ -2,12 +2,22 @@ package com.tamtdk.tam.xesbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView brow; // initialize
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        brow = (WebView)findViewById(R.id.wv_view); // declare
+//        String url = "http://192.168.10.44:8888";
+//        String url = "http://www.hackstories.com";
+        String url = "https://www.whatismybrowser.com";
+        brow.loadUrl(url);
     }
 }
